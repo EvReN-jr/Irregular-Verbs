cf69d01c370764a3fd653516709f52b433c6740e
EVREN <54286699+EvReN-jr@users.noreply.github.com>
2020-02-03 14:26:24 +0300

    Create Game.Desinger.cs

---
+++ Game.Desinger.cs
@@ -0,0 +1,318 @@
+namespace ING_PERFORMANS
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txt_area2 = new System.Windows.Forms.TextBox();
+            this.txt_area1 = new System.Windows.Forms.TextBox();
+            this.btn_checked = new System.Windows.Forms.Button();
+            this.btn_pass = new System.Windows.Forms.Button();
+            this.btn_inf = new System.Windows.Forms.Button();
+            this.lbl_v3 = new System.Windows.Forms.Label();
+            this.lbl_v2 = new System.Windows.Forms.Label();
+            this.lbl_v = new System.Windows.Forms.Label();
+            this.lbl_point = new System.Windows.Forms.Label();
+            this.lbl_h3 = new System.Windows.Forms.Label();
+            this.lbl_v1 = new System.Windows.Forms.Label();
+            this.lbl_h22 = new System.Windows.Forms.Label();
+            this.lbl_h21 = new System.Windows.Forms.Label();
+            this.lbl_h1 = new System.Windows.Forms.Label();
+            this.lst_V = new System.Windows.Forms.ListBox();
+            this.lst_v3 = new System.Windows.Forms.ListBox();
+            this.lst_v2 = new System.Windows.Forms.ListBox();
+            this.tm_inf = new System.Windows.Forms.Timer(this.components);
+            this.btn_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_area2
+            // 
+            this.txt_area2.Location = new System.Drawing.Point(812, 394);
+            this.txt_area2.Multiline = true;
+            this.txt_area2.Name = "txt_area2";
+            this.txt_area2.Size = new System.Drawing.Size(310, 71);
+            this.txt_area2.TabIndex = 64;
+            this.txt_area2.Visible = false;
+            // 
+            // txt_area1
+            // 
+            this.txt_area1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_area1.ForeColor = System.Drawing.Color.Black;
+            this.txt_area1.Location = new System.Drawing.Point(252, 394);
+            this.txt_area1.Multiline = true;
+            this.txt_area1.Name = "txt_area1";
+            this.txt_area1.Size = new System.Drawing.Size(310, 71);
+            this.txt_area1.TabIndex = 63;
+            this.txt_area1.Visible = false;
+            // 
+            // btn_checked
+            // 
+            this.btn_checked.Font = new System.Drawing.Font("Calisto MT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_checked.Location = new System.Drawing.Point(604, 633);
+            this.btn_checked.Name = "btn_checked";
+            this.btn_checked.Size = new System.Drawing.Size(158, 61);
+            this.btn_checked.TabIndex = 62;
+            this.btn_checked.Text = "CHECKED";
+            this.btn_checked.UseVisualStyleBackColor = true;
+            this.btn_checked.Visible = false;
+            this.btn_checked.Click += new System.EventHandler(this.btn_checked_Click);
+            // 
+            // btn_pass
+            // 
+            this.btn_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_pass.Location = new System.Drawing.Point(12, 633);
+            this.btn_pass.Name = "btn_pass";
+            this.btn_pass.Size = new System.Drawing.Size(105, 61);
+            this.btn_pass.TabIndex = 61;
+            this.btn_pass.Text = "PASS";
+            this.btn_pass.UseVisualStyleBackColor = true;
+            this.btn_pass.Visible = false;
+            this.btn_pass.Click += new System.EventHandler(this.btn_pass_Click);
+            // 
+            // btn_inf
+            // 
+            this.btn_inf.BackColor = System.Drawing.Color.Blue;
+            this.btn_inf.Font = new System.Drawing.Font("Monotype Corsiva", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_inf.ForeColor = System.Drawing.Color.White;
+            this.btn_inf.Location = new System.Drawing.Point(1261, 642);
+            this.btn_inf.Name = "btn_inf";
+            this.btn_inf.Size = new System.Drawing.Size(52, 52);
+            this.btn_inf.TabIndex = 60;
+            this.btn_inf.Text = "i";
+            this.btn_inf.UseVisualStyleBackColor = false;
+            this.btn_inf.Click += new System.EventHandler(this.btn_inf_Click);
+            // 
+            // lbl_v3
+            // 
+            this.lbl_v3.AutoSize = true;
+            this.lbl_v3.Font = new System.Drawing.Font("Microsoft Sans Serif", 1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_v3.Location = new System.Drawing.Point(177, 60);
+            this.lbl_v3.Name = "lbl_v3";
+            this.lbl_v3.Size = new System.Drawing.Size(6, 2);
+            this.lbl_v3.TabIndex = 59;
+            this.lbl_v3.Text = "label9";
+            this.lbl_v3.Visible = false;
+            // 
+            // lbl_v2
+            // 
+            this.lbl_v2.AutoSize = true;
+            this.lbl_v2.Font = new System.Drawing.Font("Microsoft Sans Serif", 1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_v2.Location = new System.Drawing.Point(177, 49);
+            this.lbl_v2.Name = "lbl_v2";
+            this.lbl_v2.Size = new System.Drawing.Size(6, 2);
+            this.lbl_v2.TabIndex = 58;
+            this.lbl_v2.Text = "label8";
+            this.lbl_v2.Visible = false;
+            // 
+            // lbl_v
+            // 
+            this.lbl_v.AutoSize = true;
+            this.lbl_v.Font = new System.Drawing.Font("Microsoft Sans Serif", 1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_v.Location = new System.Drawing.Point(177, 39);
+            this.lbl_v.Name = "lbl_v";
+            this.lbl_v.Size = new System.Drawing.Size(6, 2);
+            this.lbl_v.TabIndex = 57;
+            this.lbl_v.Text = "label7";
+            this.lbl_v.Visible = false;
+            // 
+            // lbl_point
+            // 
+            this.lbl_point.AutoSize = true;
+            this.lbl_point.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_point.Location = new System.Drawing.Point(1295, 37);
+            this.lbl_point.Name = "lbl_point";
+            this.lbl_point.Size = new System.Drawing.Size(55, 39);
+            this.lbl_point.TabIndex = 56;
+            this.lbl_point.Text = "00";
+            this.lbl_point.Visible = false;
+            // 
+            // lbl_h3
+            // 
+            this.lbl_h3.AutoSize = true;
+            this.lbl_h3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_h3.Location = new System.Drawing.Point(1167, 37);
+            this.lbl_h3.Name = "lbl_h3";
+            this.lbl_h3.Size = new System.Drawing.Size(122, 39);
+            this.lbl_h3.TabIndex = 55;
+            this.lbl_h3.Text = "POINT";
+            this.lbl_h3.Visible = false;
+            // 
+            // lbl_v1
+            // 
+            this.lbl_v1.AutoSize = true;
+            this.lbl_v1.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_v1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_v1.ForeColor = System.Drawing.Color.Black;
+            this.lbl_v1.Location = new System.Drawing.Point(640, 184);
+            this.lbl_v1.Name = "lbl_v1";
+            this.lbl_v1.Size = new System.Drawing.Size(143, 54);
+            this.lbl_v1.TabIndex = 54;
+            this.lbl_v1.Text = "NULL";
+            this.lbl_v1.Visible = false;
+            // 
+            // lbl_h22
+            // 
+            this.lbl_h22.AutoSize = true;
+            this.lbl_h22.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_h22.Font = new System.Drawing.Font("Monotype Corsiva", 45F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_h22.ForeColor = System.Drawing.Color.Black;
+            this.lbl_h22.Location = new System.Drawing.Point(800, 247);
+            this.lbl_h22.Name = "lbl_h22";
+            this.lbl_h22.Size = new System.Drawing.Size(501, 72);
+            this.lbl_h22.TabIndex = 53;
+            this.lbl_h22.Text = "PAST PARTICIPLE";
+            this.lbl_h22.Visible = false;
+            // 
+            // lbl_h21
+            // 
+            this.lbl_h21.AutoSize = true;
+            this.lbl_h21.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_h21.Font = new System.Drawing.Font("Monotype Corsiva", 45F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_h21.ForeColor = System.Drawing.Color.Black;
+            this.lbl_h21.Location = new System.Drawing.Point(240, 247);
+            this.lbl_h21.Name = "lbl_h21";
+            this.lbl_h21.Size = new System.Drawing.Size(386, 72);
+            this.lbl_h21.TabIndex = 52;
+            this.lbl_h21.Text = "PAST SIMPLE";
+            this.lbl_h21.Visible = false;
+            // 
+            // lbl_h1
+            // 
+            this.lbl_h1.AutoSize = true;
+            this.lbl_h1.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_h1.Font = new System.Drawing.Font("Kristen ITC", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_h1.ForeColor = System.Drawing.Color.Black;
+            this.lbl_h1.Location = new System.Drawing.Point(495, 101);
+            this.lbl_h1.Name = "lbl_h1";
+            this.lbl_h1.Size = new System.Drawing.Size(424, 83);
+            this.lbl_h1.TabIndex = 51;
+            this.lbl_h1.Text = "BASE FORM";
+            this.lbl_h1.Visible = false;
+            // 
+            // lst_V
+            // 
+            this.lst_V.FormattingEnabled = true;
+            this.lst_V.Location = new System.Drawing.Point(12, 37);
+            this.lst_V.Name = "lst_V";
+            this.lst_V.Size = new System.Drawing.Size(138, 4);
+            this.lst_V.TabIndex = 50;
+            this.lst_V.Visible = false;
+            // 
+            // lst_v3
+            // 
+            this.lst_v3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lst_v3.FormattingEnabled = true;
+            this.lst_v3.ItemHeight = 25;
+            this.lst_v3.Location = new System.Drawing.Point(12, 60);
+            this.lst_v3.Name = "lst_v3";
+            this.lst_v3.Size = new System.Drawing.Size(138, 4);
+            this.lst_v3.TabIndex = 49;
+            this.lst_v3.Visible = false;
+            // 
+            // lst_v2
+            // 
+            this.lst_v2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lst_v2.FormattingEnabled = true;
+            this.lst_v2.ItemHeight = 25;
+            this.lst_v2.Location = new System.Drawing.Point(12, 47);
+            this.lst_v2.Name = "lst_v2";
+            this.lst_v2.Size = new System.Drawing.Size(138, 4);
+            this.lst_v2.TabIndex = 48;
+            this.lst_v2.Visible = false;
+            // 
+            // tm_inf
+            // 
+            this.tm_inf.Interval = 1000;
+            this.tm_inf.Tick += new System.EventHandler(this.tm_inf_Tick);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold);
+            this.btn_start.Location = new System.Drawing.Point(556, 306);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(261, 92);
+            this.btn_start.TabIndex = 65;
+            this.btn_start.Text = "START";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1350, 730);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.txt_area2);
+            this.Controls.Add(this.txt_area1);
+            this.Controls.Add(this.btn_checked);
+            this.Controls.Add(this.btn_pass);
+            this.Controls.Add(this.btn_inf);
+            this.Controls.Add(this.lbl_v3);
+            this.Controls.Add(this.lbl_v2);
+            this.Controls.Add(this.lbl_v);
+            this.Controls.Add(this.lbl_point);
+            this.Controls.Add(this.lbl_h3);
+            this.Controls.Add(this.lbl_v1);
+            this.Controls.Add(this.lbl_h22);
+            this.Controls.Add(this.lbl_h21);
+            this.Controls.Add(this.lbl_h1);
+            this.Controls.Add(this.lst_V);
+            this.Controls.Add(this.lst_v3);
+            this.Controls.Add(this.lst_v2);
+            this.Name = "Game";
+            this.Text = "IRREGULAR VERBS GAME";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_area2;
+        private System.Windows.Forms.TextBox txt_area1;
+        private System.Windows.Forms.Button btn_checked;
+        private System.Windows.Forms.Button btn_pass;
+        private System.Windows.Forms.Button btn_inf;
+        private System.Windows.Forms.Label lbl_v3;
+        private System.Windows.Forms.Label lbl_v2;
+        private System.Windows.Forms.Label lbl_v;
+        private System.Windows.Forms.Label lbl_point;
+        private System.Windows.Forms.Label lbl_h3;
+        private System.Windows.Forms.Label lbl_v1;
+        private System.Windows.Forms.Label lbl_h22;
+        private System.Windows.Forms.Label lbl_h21;
+        private System.Windows.Forms.Label lbl_h1;
+        private System.Windows.Forms.ListBox lst_V;
+        private System.Windows.Forms.ListBox lst_v3;
+        private System.Windows.Forms.ListBox lst_v2;
+        private System.Windows.Forms.Timer tm_inf;
+        private System.Windows.Forms.Button btn_start;
+    }
+}
